19960fd61bb17911f294665ebabe7de979657be8
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-05-16 22:08:57 -0400

    fix imports

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,34 @@
-import { lazy } from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Root from './Root';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Suspense, lazy } from 'react';
+import {
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+  useNavigation,
+} from 'react-router-dom';
+import LoadingOverlay from './components/ui/LoadingOverlay';
+import { Toaster } from './components/ui/Toaster';
+import queryClient from './services/queryClient';
 
-const HomePage = lazy(() => import('./HomePage/HomePage'));
-const PostDetailsPage = lazy(() => import('./PostDetailsPage/PostDetailsPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const PostDetailsPage = lazy(
+  () => import('./pages/PostDetailsPage/PostDetailsPage'),
+);
+
+const Root = () => {
+  const navigation = useNavigation();
+  const isLoadingPage = navigation.state === 'loading';
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LoadingOverlay isLoading={isLoadingPage} />
+      <Toaster />
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </QueryClientProvider>
+  );
+};
 
 const router = createBrowserRouter([
   {
