8c6c9c315f860f4537d425716b8ff45471d13c92
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-05-17 22:26:31 -0400

    loader and clientloader

--- app/routes/posts.$id.tsx
+++ app/routes/posts.$id.tsx
@@ -1,13 +1,45 @@
-import { json, useLoaderData } from '@remix-run/react';
+import {
+  ClientLoaderFunctionArgs,
+  json,
+  useLoaderData,
+} from '@remix-run/react';
 import PageWrapper from '@/components/app/PageWrapper';
 import { LoaderFunctionArgs } from '@remix-run/node';
 import postsService from '@/services/postsService';
+import queryClient from '@/services/queryClient';
+import QueryKey from '@/services/queries/QueryKey';
+import Post from '@/types/Post';
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const post = await postsService.getPost(params?.id ?? '');
 
   return json({ post });
 };
+
+export const clientLoader = async ({
+  serverLoader,
+  params,
+}: ClientLoaderFunctionArgs) => {
+  const cachedPost = queryClient.getQueryData<Post>([
+    QueryKey.GET_POST,
+    params?.id,
+  ]);
+
+  if (cachedPost) {
+    return json({ post: cachedPost });
+  }
+
+  const post = await queryClient.fetchQuery({
+    queryKey: [QueryKey.GET_POSTS, params?.id],
+    queryFn: () => serverLoader<Post>(),
+  });
+
+  return json({
+    post,
+  });
+};
+
+clientLoader.hydrate = true;
 
 const PostDetailsPage = () => {
   const { post } = useLoaderData<typeof loader>();
