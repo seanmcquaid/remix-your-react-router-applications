4be56c56ce1f622867238d6a69d20923779b442c
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-06-14 15:38:45 -0400

    spa mode and ssr

---
+++ app/root.tsx
@@ -0,0 +1,42 @@
+import { Links, Meta, Outlet, Scripts, useNavigation } from '@remix-run/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
+import LoadingOverlay from './components/ui/LoadingOverlay';
+import { Toaster } from './components/ui/Toaster';
+import queryClient from './services/queryClient';
+import './styles/globals.css';
+
+export const Layout = ({ children }: PropsWithChildren) => {
+  const navigation = useNavigation();
+  const isLoadingPage = navigation.state === 'loading';
+
+  return (
+    <html lang="en" className="h-screen w-full">
+      <head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Vite + Remix + TS</title>
+        <Links />
+        <Meta />
+      </head>
+      <body className="h-screen w-full">
+        <div id="root">
+          <QueryClientProvider client={queryClient}>
+            <LoadingOverlay isLoading={isLoadingPage} />
+            <Toaster />
+            {children}
+          </QueryClientProvider>
+        </div>
+        <Scripts />
+        <noscript> You need to enable JavaScript to run this app! :( </noscript>
+      </body>
+    </html>
+  );
+};
+
+const Root = () => {
+  return <Outlet />;
+};
+
+export default Root;
