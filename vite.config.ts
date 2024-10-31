dc5ca10a703394af53e538a6f21261c515b18d73
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-10-31 14:10:34 -0400

    ssr to true

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { reactRouter } from '@react-router/dev/vite';
 export default defineConfig({
   plugins: [
     reactRouter({
-      ssr: false,
+      ssr: true,
     }),
     checker({ typescript: true }),
     tsconfigPaths(),
