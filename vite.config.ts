c4701a8d0f20d4a6b6491890af4ff0e673adf80e
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-11-05 17:09:43 -0500

    enable ssr

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     reactRouter({
-      ssr: false,
+      ssr: true,
     }),
     checker({ typescript: true }),
     tsconfigPaths(),
