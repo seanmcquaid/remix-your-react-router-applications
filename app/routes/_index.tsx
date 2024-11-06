d196242de932ba8e2ba4da3c916a479e559cd2be
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-11-06 15:08:44 -0500

    pe

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,54 +2,97 @@ import PageWrapper from '@/components/app/PageWrapper';
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import LinkButton from '@/components/ui/LinkButton';
-import useDeletePostMutation from '@/services/mutations/useDeletePostMutation';
-import useGetPostsQuery from '@/services/queries/useGetPostsQuery';
-import { ChangeEvent, FormEvent, useState } from 'react';
-import { useSearchParams } from 'react-router';
+import postsService from '@/services/postsService';
+import { Form, useFetcher, useSearchParams } from 'react-router';
+import { Route } from './+types._index';
+import queryClient from '@/services/queryClient';
+import QueryKey from '@/services/QueryKey';
+import { toast } from '@/hooks/useToast';
 
-const HomePage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchTerm, setSearchTerm] = useState('');
-  const { data, isLoading, isError } = useGetPostsQuery();
-  const { mutate: deletePost, isPending: deletePostLoading } =
-    useDeletePostMutation();
+export const loader = async () => {
+  const posts = await postsService.getPosts();
+
+  return posts;
+};
+
+export const clientLoader = async ({
+  serverLoader,
+}: Route.ClientLoaderArgs) => {
+  const posts = await queryClient.ensureQueryData({
+    queryKey: [QueryKey.GET_POSTS],
+    queryFn: () => serverLoader(),
+  });
+
+  return posts;
+};
+
+clientLoader.hydrate = true;
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const intent = formData.get('intent') as string;
+
+  if (intent === 'delete') {
+    const id = formData.get('id') as string;
+    await postsService.deletePost(id);
+    return null;
+  }
+
+  return null;
+};
+
+export const clientAction = async ({
+  request,
+  serverAction,
+}: Route.ClientActionArgs) => {
+  const method = request.method;
+
+  if (method === 'DELETE') {
+    await serverAction();
+    queryClient.removeQueries({
+      queryKey: [QueryKey.GET_POSTS],
+    });
+    toast({
+      title: 'Post deleted',
+    });
+    return null;
+  }
+
+  return null;
+};
+
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const [searchParams] = useSearchParams();
   const search = searchParams.get('search') ?? '';
   const filteredPosts =
-    data?.filter(post =>
+    loaderData?.filter(post =>
       post.title.toLowerCase().includes(search.toLowerCase()),
     ) ?? [];
-
-  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setSearchParams({ search: searchTerm });
-  };
+  const fetcher = useFetcher();
+  const isDeleting = fetcher.state !== 'idle';
 
   return (
-    <PageWrapper isLoading={isLoading} isError={isError}>
-      <form onSubmit={handleOnSubmit}>
-        <Input
-          type="text"
-          name="search"
-          onChange={handleOnChange}
-          value={searchTerm}
-        />
+    <PageWrapper>
+      <Form>
+        <Input type="text" name="search" defaultValue={search} />
         <Button type="submit">Search</Button>
-      </form>
+      </Form>
       <ul className="grid grid-cols-2">
         {filteredPosts?.map(post => (
           <li key={post.id} className="flex mt-4 items-center">
-            {post.title.substring(0, 5)}
-            <Button
-              onClick={() => deletePost(post.id.toString())}
-              disabled={deletePostLoading}
-              className="ml-4"
-            >
-              Delete
-            </Button>
+            <fetcher.Form method="DELETE">
+              {post.title.substring(0, 5)}
+              <input type="hidden" name="id" value={post.id} />
+              <Button
+                type="submit"
+                name="intent"
+                value="delete"
+                className="ml-4"
+                disabled={isDeleting}
+              >
+                Delete
+              </Button>
+            </fetcher.Form>
             <LinkButton to={`posts/${post.id}`} className="ml-4">
               View
             </LinkButton>
--- app/routes/posts.$id.tsx
+++ app/routes/posts.$id.tsx
@@ -1,19 +1,38 @@
-import { useParams } from 'react-router';
 import PageWrapper from '@/components/app/PageWrapper';
-import useGetPostQuery from '@/services/queries/useGetPostQuery';
 import LinkButton from '@/components/ui/LinkButton';
+import { Route } from './+types.posts.$id';
+import postsService from '@/services/postsService';
+import queryClient from '@/services/queryClient';
+import QueryKey from '@/services/QueryKey';
 
-const PostDetailsPage = () => {
-  const { id } = useParams();
-  const { data, isLoading, isError } = useGetPostQuery(id!);
+export const loader = async ({ params }: Route.LoaderArgs) => {
+  const post = await postsService.getPost(params.id as string);
 
+  return post;
+};
+
+export const clientLoader = async ({
+  params,
+  serverLoader,
+}: Route.ClientLoaderArgs) => {
+  const post = await queryClient.ensureQueryData({
+    queryKey: [QueryKey.GET_POST, params.id],
+    queryFn: () => serverLoader(),
+  });
+
+  return post;
+};
+
+clientLoader.hydrate = true;
+
+const PostDetailsPage = ({ loaderData }: Route.ComponentProps) => {
   return (
-    <PageWrapper isLoading={isLoading} isError={isError}>
+    <PageWrapper>
       <LinkButton to="/" className="m-4">
         Back to Posts
       </LinkButton>
-      <h1>{data?.title}</h1>
-      <p>{data?.body}</p>
+      <h1>{loaderData?.title}</h1>
+      <p>{loaderData?.body}</p>
     </PageWrapper>
   );
 };
