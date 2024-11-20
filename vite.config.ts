ec78d461a868dd302e8639f03b706ff4bad10f7d
seanmcquaid <sean.m.mcquaid@gmail.com>
2024-11-20 12:06:29 -0500

    react router v7 ssr start

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
+  plugins: [reactRouter(), checker({ typescript: true }), tsconfigPaths()],
   preview: {
     port: 3000,
     open: true,
